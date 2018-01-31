fe204601466ca1d3d13bd4e4652ad017fac9a7f5
Yiftah <yift.shl2@gmail.com>
2018-01-31 17:32:26 +0200

    main screen replaced by a screen for server details

---
+++ MyLocation/app/src/main/java/com/shlezy/mylocation/ConnectionActivity.java
@@ -0,0 +1,16 @@
+package com.shlezy.mylocation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnectionActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection);
+        //TODO: handle connection to server using intent extras from MainActivity
+    }
+}
