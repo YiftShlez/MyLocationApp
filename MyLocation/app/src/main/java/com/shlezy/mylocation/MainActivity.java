a75e92f4f4f1d201f69877d5091efe63a0368434
Yiftah <yift.shl2@gmail.com>
2018-02-11 14:38:02 +0200

    Added option for saving server details in MainActivity

--- MyLocation/app/src/main/java/com/shlezy/mylocation/MainActivity.java
+++ MyLocation/app/src/main/java/com/shlezy/mylocation/MainActivity.java
@@ -1,10 +1,13 @@
 package com.shlezy.mylocation;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,6 +18,11 @@ public class MainActivity extends AppCompatActivity
     public static final String serverIPExtra = "ServerIP";
     public static final String serverPortExtra = "ServerPort";
     public static final String TAG = "com.shlezy.mylocation";
+    public static final String ipPrefKey = "serverIP";
+    public static final String portPrefKey = "serverPort";
+    private SharedPreferences sharedPref = null;
+    private CheckBox rememberBox = null;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
@@ -22,11 +30,23 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         ipEdit = findViewById(R.id.main_edit_ip);
         portEdit = findViewById(R.id.main_edit_port);
+        rememberBox = findViewById(R.id.main_check_remember);
+        sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        String serverIP = sharedPref.getString(ipPrefKey, "");
+        int serverPort = sharedPref.getInt(portPrefKey, 0);
+        if (serverIP.length() > 0 && serverPort > 0)
+        {
+            ipEdit.setText(serverIP);
+            ipEdit.setText("" + serverPort);
+        }
     }
 
-    public void connect (View view)
+    public void connect(View view)
     {
-        Intent intent = new Intent (this, MapActivity.class);
+        if (rememberBox.isChecked())
+            sharedPref.edit().putString(ipPrefKey, ipEdit.getText().toString())
+                    .putInt(portPrefKey, Integer.parseInt(portEdit.getText().toString()));
+        Intent intent = new Intent(this, MapActivity.class);
         intent.putExtra(serverIPExtra, ipEdit.getText().toString());
         intent.putExtra(serverPortExtra, Integer.parseInt(portEdit.getText().toString()));
         startActivity(intent);
